67a49ebdd8d109c68df54ea615ec8df732cb9a86
Joao Ramos <joao.candido@etu.unige.ch>
2024-04-17 00:22:29 +0200

    update main to add render

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from milo.env import make_env
+import numpy as np
 
 # gymnasium
-env = make_env("Humanoid-v5", num_envs=3, vectorization_mode="async")
+env = make_env("Humanoid-v5", num_envs=3, vectorization_mode="async", env_spec_kwargs={"render_mode": "rgb_array"})
 obs, info = env.reset(seed=13)
-print(obs)
+imgs = env.render()
+print(obs.shape, len(imgs), imgs[0].shape, imgs[1].shape, imgs[2].shape)
 
 # metaworld
 env = make_env("button-press-topdown-v2", num_envs=2, vectorization_mode="sync")
 obs, info = env.reset(seed=13)
-print(obs)
+imgs = env.render()
+print(obs.shape, len(imgs), imgs[0].shape, imgs[1].shape)
 
 # dmc
 env = make_env("walker-walk", num_envs=3, vectorization_mode="sync")
 obs, info = env.reset(seed=13)
-print(obs)
+for i in range(10):
+    obs, _, _, _, _ = env.step(env.action_space.sample())
+    imgs = env.render()
+    print(i, obs.shape, len(imgs), imgs[0].shape, imgs[1].shape, imgs[2].shape)
