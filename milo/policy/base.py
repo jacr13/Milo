17da54eb7fa36c406d91cc29b51951b6f7ed258f
Joao Ramos <joao.candido@etu.unige.ch>
2024-05-02 16:44:45 +0200

    add template for ppo and base class

---
+++ milo/policy/base.py
@@ -0,0 +1,36 @@
+from typing import Literal
+
+import torch.nn as nn
+
+
+class BasePolicy(nn.Module):
+    def __init__(
+        self,
+        *,
+        action_space,
+        observation_space: None = None,
+        action_scaling: bool = False,
+        action_bound_method: Literal["clip", "tanh"] | None = "clip",
+        lr_scheduler: None = None,
+    ) -> None:
+        self._action_type: Literal["discrete", "continuous"] = "discrete"
+
+    @property
+    def action_type(self) -> Literal["discrete", "continuous"]:
+        return self._action_type
+
+    def compute_action(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def learn(self):
+        pass
+
+    def update(self):
+        pass
+
+    @staticmethod
+    def compute_episodic_return():
+        return
---
+++ milo/policy/modelfree/ppo.py
@@ -0,0 +1,8 @@
+class PPOPolicy:
+    def __init__(self):
+        self.model = None
+        self.optimizer = None
+        self.action_space = None
+
+    def learn(self):
+        pass
