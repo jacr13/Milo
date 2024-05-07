025793e93a37b70934a8923032470a20039e7967
Joao Ramos <joao.candido@etu.unige.ch>
2024-05-07 18:49:19 +0200

    add torch to batch and specialised batches for obs and acs

--- milo/data/batch.py
+++ milo/data/batch.py
@@ -1,30 +1,57 @@
-import numpy as np
+import warnings
+from typing import Any, Literal
 
-try:
-    import torch
-except ImportError:
-    torch = None  # type: ignore
+import numpy as np
+import torch
+
+from milo.data.transition import Transition
 
 
 class Batch:
-    obs: np.ndarray | None = None
-    action: np.ndarray | None = None
-    reward: np.ndarray | None = None
-    next_obs: np.ndarray | None = None
-    done: np.ndarray | None = None
-    terminated: np.ndarray | None = None
-    truncated: np.ndarray | None = None
-    info: np.ndarray | None = None
-    pixels: np.ndarray | None = None
+    obs: np.ndarray | torch.Tensor | None = None
+    action: np.ndarray | torch.Tensor | None = None
+    reward: np.ndarray | torch.Tensor | None = None
+    next_obs: np.ndarray | torch.Tensor | None = None
+    done: np.ndarray | torch.Tensor | None = None
+    terminated: np.ndarray | torch.Tensor | None = None
+    truncated: np.ndarray | torch.Tensor | None = None
+    info: list | None = None
+    pixels: np.ndarray | torch.Tensor | None = None
 
-    def __init__(self, batch: list, exclude: list | None = None, only: list | None = None) -> None:
+    def __init__(
+        self,
+        data: list | dict,
+        batch_type: Literal["torch", "numpy"] | None = None,
+        device: torch.device | None = None,
+        exclude: list | None = None,
+        only: list | None = None,
+    ) -> None:
         assert exclude is None or only is None, "Cannot specify both exclude and only keys."
         exclude = exclude or []
         only = only or []
 
+        self._device = device or torch.device("cpu")
         self._keys = self._setup_keys(exclude, only)
-        self._batch = batch
+        self._data = data
+        self._batch_type = batch_type or self._get_batch_type(data)
         self._setup(set_attr=True)
+
+    def _get_batch_type(self, data: list | dict) -> Literal["torch", "numpy"]:
+        data_example = None
+        if isinstance(data, list) and isinstance(data[0], Transition):
+            data_example = data[0].obs
+        if isinstance(data, dict):
+            key = next(iter(data.keys()))
+            data_example = data[key]
+
+        if data_example is None:
+            raise ValueError("Data should be a list of transitions or a dictionary of arrays.")
+
+        if isinstance(data_example, torch.Tensor):
+            return "torch"
+        if isinstance(data_example, np.ndarray):
+            return "numpy"
+        raise ValueError("Data should be a list of transitions or a dictionary of arrays.")
 
     def _setup_keys(self, exclude: list, only: list) -> list:
         if only:
@@ -43,17 +70,33 @@ class Batch:
         setattr(self, key, value)
 
     def _setup(self, set_attr: bool = False) -> dict:
-        keys = list(self._batch[0].__dict__.keys())
+        batch_dict = {}
+        if isinstance(self._data, list) and isinstance(self._data[0], Transition):
+            # Check if all transitions have the same attributes
+            for transition in self._data[1:]:
+                assert set(self._keys).issubset(
+                    set(transition.__dict__.keys()),
+                ), f"Transitions should have at least the following keys: {self._keys}, but got {set(transition.__dict__.keys())}"
 
-        # Check if all transitions have the same attributes
-        for transition in self._batch[1:]:
+            # Stack the attributes of the transitions in numpy arrays
+            batch_dict = {key: [getattr(transition, key) for transition in self._data] for key in self._keys}
+        elif isinstance(self._data, dict):
             assert set(self._keys).issubset(
-                set(transition.__dict__.keys()),
-            ), f"Transitions should have at least the following keys: {self._keys}"
+                self._data.keys(),
+            ), f"Data should have at least the following keys: {self._keys}, but got {self._data.keys()}"
+            batch_dict = {key: self._data[key] for key in self._keys}
+        else:
+            raise ValueError(f"Unsupported data type: {type(self._data)}")
 
-        # Stack the attributes of the transitions in numpy arrays
-        # TODO: this should depend on the type of the transitions received, eg. numpy arrays or torch tensors
-        batch_dict = {key: np.stack([getattr(transition, key) for transition in self._batch]) for key in keys}
+        for key, value in batch_dict.items():
+            print(key, type(value))
+
+        if self._batch_type == "numpy":
+            batch_dict = {key: np.stack(value) for key, value in batch_dict.items()}
+        elif self._batch_type == "torch":
+            batch_dict = {
+                key: torch.stack(value) if isinstance(value, list) else value for key, value in batch_dict.items()
+            }
 
         # Set the attributes of the Batch object to the numpy arrays (optional)
         if set_attr:
@@ -76,12 +119,16 @@ class Batch:
             else:
                 self.__dict__[key] = np.array(self.__dict__[key])
 
-    def to_torch(self, device: str = "cpu", exclude: list | None = None, only: list | None = None) -> None:
+        self._batch_type = "numpy"
+
+    def to_torch(
+        self, device: torch.device | None = None, exclude: list | None = None, only: list | None = None,
+    ) -> None:
         assert only is None or exclude is None, "Cannot specify both exclude and only keys."
         exclude = exclude or ["info"]
         keys = only or list(self.__dict__.keys())
 
-        assert torch is not None, "PyTorch is not installed"
+        device = device or torch.device("cpu")
 
         for key in keys:
             # Skip the keys in exclude
@@ -89,7 +136,7 @@ class Batch:
                 continue
 
             if isinstance(self.__dict__[key], torch.Tensor):
-                continue
+                self.__dict__[key] = self.__dict__[key].to(device)
 
             if isinstance(self.__dict__[key], np.ndarray):
                 # Check if the dtype is not an object (impossible to convert to torch)
@@ -104,11 +151,45 @@ class Batch:
                 except TypeError as exc:
                     raise ValueError(f"Unknown type: {type(self.__dict__[key])}") from exc
 
+        self._batch_type = "torch"
+
+    def to(self, device: torch.device) -> None:
+        if self._batch_type == "numpy":
+            warnings.warn("Batch is in numpy format, it will be converted to torch format automatically.")
+
+        self.to_torch(device=device)
+
     def __len__(self) -> int:
-        return len(self._batch)
+        return len(self._data)
 
     def __repr__(self) -> str:
         attributes = ",\n".join(
             [f"  {key} = {self.__dict__[key].shape if self.__dict__[key] is not None else None}" for key in self._keys],
         )
         return f"Batch(\n{attributes}\n)"
+
+
+class BatchObs(Batch):
+    """Batch of observations."""
+    def __init__(self, obs: np.ndarray | torch.Tensor, **kwargs: Any) -> None:
+        super().__init__({"obs": obs}, only=["obs"], **kwargs)
+
+    def __repr__(self) -> str:
+        assert self._keys == ["obs"], "Only the 'obs' key is supported."
+        attributes = ",\n".join(
+            [f"  {key} = {self.__dict__[key].shape if self.__dict__[key] is not None else None}" for key in self._keys],
+        )
+        return f"BatchObs(\n{attributes}\n)"
+
+
+class BatchAction(Batch):
+    """Batch of actions."""
+    def __init__(self, action: np.ndarray | torch.Tensor, **kwargs: Any) -> None:
+        super().__init__({"action": action}, only=["action"], **kwargs)
+
+    def __repr__(self) -> str:
+        assert self._keys == ["action"], "Only the 'action' key is supported."
+        attributes = ",\n".join(
+            [f"  {key} = {self.__dict__[key].shape if self.__dict__[key] is not None else None}" for key in self._keys],
+        )
+        return f"BatchAction(\n{attributes}\n)"
